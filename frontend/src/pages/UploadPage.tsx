472fe04b1ed7713b50d6a4c363a4f58b73374871
Gazakof <randriamorasata08@gmail.com>
2026-04-19 20:43:45 +0300

    pages/uploadpage

---
+++ frontend/src/pages/UploadPage.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { uploadFile } from "../services/analysisService";
+
+const UploadPage = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState<string>("");
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
+      setMessage("");
+    }
+  };
+};
